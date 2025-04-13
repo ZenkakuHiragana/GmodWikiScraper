f6e22ce468cb843255d61e4441a684fcf78f35f6
Zenkaku Hiragana <daiko1997@yahoo.co.jp>
2025-04-13 22:31:30 +0900

    Added operators and fields for Vector, Angle, VMatrix, and Color

--- constants-sv.lua
+++ constants-sv.lua
@@ -99,3 +99,46 @@ color_black = Color(0, 0, 0, 255)
 --- = Color(255, 255, 255, 0)
 ---@type Color
 color_transparent = Color(255, 255, 255, 0)
+
+---@class Angle
+---@field x number The pitch component of the angle.
+---@field y number The yaw component of the angle.
+---@field z number The roll  component of the angle.
+---@field [1] number The pitch component of the angle.
+---@field [2] number The yaw component of the angle.
+---@field [3] number The roll  component of the angle.
+---@field p number The pitch component of the angle.
+-- ---@field y number The yaw component of the angle.
+---@field r number The roll  component of the angle.
+---@field pitch number The pitch component of the angle.
+---@field yaw   number The yaw component of the angle.
+---@field roll  number The roll  component of the angle.
+---@operator add(Angle): Angle # Returns new [Angle](https://wiki.facepunch.com/gmod/Angle) with the result of addition.
+---@operator div(number): Angle # Returns new [Angle](https://wiki.facepunch.com/gmod/Angle) with the result of division.
+---@operator mul(number): Angle # Returns new [Angle](https://wiki.facepunch.com/gmod/Angle) with the result of multiplication.
+---@operator sub(Angle): Angle # Returns new [Angle](https://wiki.facepunch.com/gmod/Angle) with the result of subtraction.
+---@operator unm: Angle # Returns new [Angle](https://wiki.facepunch.com/gmod/Angle) with the result of negation.
+
+---@class Color
+---@field r number The red component of the color.
+---@field g number The green component of the color.
+---@field b number The blue component of the color.
+---@field a number The alpha component of the color.
+
+---@class Vector
+---@field x number The X component of the vector.
+---@field y number The Y component of the vector.
+---@field z number The Z component of the vector.
+---@field [1] number The X component of the vector.
+---@field [2] number The Y component of the vector.
+---@field [3] number The Z component of the vector.
+---@operator add(Vector): Vector # Returns new [Vector](https://wiki.facepunch.com/gmod/Vector) with the result of addition.
+---@operator div(number): Vector # Returns new [Vector](https://wiki.facepunch.com/gmod/Vector) with the result of division.
+---@operator mul(number | Vector): Vector # Returns new [Vector](https://wiki.facepunch.com/gmod/Vector) with the result of multiplication.
+---@operator sub(Vector): Vector # Returns new [Vector](https://wiki.facepunch.com/gmod/Vector) with the result of subtraction.
+---@operator unm: Vector # Returns new [Vector](https://wiki.facepunch.com/gmod/Vector) with the result of negation.
+
+---@class VMatrix
+---@operator add(VMatrix): VMatrix # Returns new [VMatrix](https://wiki.facepunch.com/gmod/VMatrix) with the result of addition.
+---@operator mul(VMatrix | Vector): VMatrix # Returns new [VMatrix](https://wiki.facepunch.com/gmod/VMatrix) or [Vector](https://wiki.facepunch.com/gmod/Vector) with the result of multiplication.
+---@operator sub(VMatrix): VMatrix # Returns new [VMatrix](https://wiki.facepunch.com/gmod/VMatrix) with the result of subtraction.
